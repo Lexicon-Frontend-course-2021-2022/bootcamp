128b90c8e0e96a9685d21c64a9ea0d168e8f7d66
Thomas Hellström <rel@xed.se>
2021-10-01 10:44:39 +0200

    Comments

--- fizzbuzz/app.js
+++ fizzbuzz/app.js
@@ -1,25 +1,33 @@
 // Input positive integer
 const inputPositiveInteger = defaultValue => {
     while (true) {
+
+        /* Get number */
         let i = Number(window.prompt("Hur många gånger ska vi iterera? Du kan bara välja ett positivt heltal.", defaultValue));
 
-        console.log(Number(i))
-
+        /* Basic sanity check */
         if (typeof i === 'number' && i >= 1 && Number.isInteger(i)) {
             return i;
         }
+
+        /* Stupid user needs a slap! */
         window.alert("Du måste mata in ett positivt heltal! Försök igen!");
+
     }
+
 }
 
-// Output results
+// Output results as html
 const fizzBuzz = iterations => {
+
+    /* Get element to put new children in */
     let list = document.querySelector("#result");
 
     for (let i = 1; i < iterations + 1; i++) {
         let result = '';
         let htmlClass = '';
 
+        /* Simple fizzbuzz algo */
         if (!(i % 3)) {
             result += "Fizz";
             htmlClass = "fizz";
@@ -33,11 +41,14 @@ const fizzBuzz = iterations => {
             htmlClass = "variable";
         }
 
+        /* Output result as a new child */
         let item = document.createElement('p');
         item.className = htmlClass;
         item.innerText = result;
         list.appendChild(item);
+
     }
+
 }
 
 // Do it! :D
